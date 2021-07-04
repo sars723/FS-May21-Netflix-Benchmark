5758c99b5209c0f48f9c9dc38416ea19e6ce6f52
sars723 <sarasalomonn@gmail.com>
2021-07-04 20:31:36 +0200

    post works

--- js/script.js
+++ js/script.js
@@ -1,83 +1,84 @@
- const comedyDiv=document.getElementById("comedy")
- const dramaDiv=document.getElementById("drama")
- const horrerDiv=document.getElementById("horrer")
-const displayComedy=(comedyMovies)=>{
-    console.log(comedyMovies)
-   
-    console.log(comedyDiv)
-    comedyDiv.innerHTML=""
-    comedyMovies.forEach(movie=>{
-        comedyDiv.innerHTML=`
+ const comedyDiv1=document.getElementById("comedy1")
+ const dramaDiv1=document.getElementById("drama1")
+ const horrerDiv1=document.getElementById("horrer1")
+ const comedyDiv2=document.getElementById("comedy2")
+ const dramaDiv2=document.getElementById("drama2")
+ const horrerDiv2=document.getElementById("horrer2")
+ const carouselDiv=document.getElementById("carousel-wrapper")
+ const dramaTitle=document.getElementById("drama-title")
+ const comedyTitle=document.getElementById("comedy-title")
+ const horrerTitle=document.getElementById("horrer-title")
+ const displayMovie=(movies,catagory)=>{
+    if(catagory==="comedy"){
+        comedyTitle.innerText=catagory
+ comedyDiv1.innerHTML=""
+    movies.filter((movie,i)=>i<6).map(movie=>{
+        comedyDiv1.innerHTML+=`
         <div class="col-md-2">
         <img class="movie-cover" src="${movie.imageUrl}" />
       </div>`
     })
-
-}
-const displayDrama=(dramaMovies)=>{
-    console.log(dramaMovies)
-   
-    console.log(dramaDiv)
-    dramaDiv.innerHTML=""
-    dramaMovies.forEach(movie=>{
-       dramaDiv.innerHTML=`
+    comedyDiv2.innerHTML=""
+    movies.filter((movie,i)=>i>=6&&i<12).map(movie=>{
+        comedyDiv2.innerHTML+=`
         <div class="col-md-2">
         <img class="movie-cover" src="${movie.imageUrl}" />
       </div>`
     })
-
-}
-const displayHorrer=(horrerMovies)=>{
-    console.log(horrerMovies)
+    }
+    else if(catagory==="drama"){
+        dramaTitle.innerText=catagory
+        dramaDiv1.innerHTML=""
+        movies.filter((movie,i)=>i<6).map(movie=>{
+            dramaDiv1.innerHTML+=`
+            <div class="col-md-2">
+            <img class="movie-cover" src="${movie.imageUrl}" />
+          </div>`
+        })
+        dramaDiv2.innerHTML=""
+        movies.filter((movie,i)=>i>=6&&i<12).map(movie=>{
+            dramaDiv2.innerHTML+=`
+            <div class="col-md-2">
+            <img class="movie-cover" src="${movie.imageUrl}" />
+          </div>`
+        })
+    }
+    else if(catagory==="horrer"){
+        horrerTitle.innerText=catagory
+        horrerDiv1.innerHTML=""
+        movies.filter((movie,i)=>i<6).map(movie=>{
+            horrerDiv1.innerHTML+=`
+            <div class="col-md-2">
+            <img class="movie-cover" src="${movie.imageUrl}" />
+          </div>`
+        })
+        horrerDiv2.innerHTML=""
+        movies.filter((movie,i)=>i>=6&&i<12).map(movie=>{
+            horrerDiv2.innerHTML+=`
+            <div class="col-md-2">
+            <img class="movie-cover" src="${movie.imageUrl}" />
+          </div>`
+        })
+    }
    
-    console.log(horrerDiv)
-    horrerDiv.innerHTML=""
-    horrerMovies.forEach(movie=>{
-        horrerDiv.innerHTML=`
-        <div class="col-md-2">
-        <img class="movie-cover" src="${movie.imageUrl}" />
-      </div>`
-    })
 
 }
-const fetchComedy=async ()=>{
- const response= await fetch("https://striveschool-api.herokuapp.com/api/movies/comedy", {
-        headers: {
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGFlM2YzMGNlYWY0ODAwMTVjOTE4NjkiLCJpYXQiOjE2MjUwMjYxOTIsImV4cCI6MTYyNjIzNTc5Mn0.PsxaF5WqdL3M99ArdsNnDDq7wUtiPxw_4Nn-WRpsftI",
-        },
-      })
-      const fetchedData=await response.json()
-      displayComedy(fetchedData)
-      console.log(fetchedData)
-}
-const fetchDrama=async ()=>{
-    const response= await fetch("https://striveschool-api.herokuapp.com/api/movies/drama", {
+
+   const fetchMovie=async (catagory)=>{
+    const response= await fetch("https://striveschool-api.herokuapp.com/api/movies/"+catagory, {
            headers: {
              Authorization:
                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGFlM2YzMGNlYWY0ODAwMTVjOTE4NjkiLCJpYXQiOjE2MjUwMjYxOTIsImV4cCI6MTYyNjIzNTc5Mn0.PsxaF5WqdL3M99ArdsNnDDq7wUtiPxw_4Nn-WRpsftI",
            },
          })
          const fetchedData=await response.json()
-         displayDrama(fetchedData)
-         console.log(fetchedData)
-   }
-   const fetchHorrer=async ()=>{
-    const response= await fetch("https://striveschool-api.herokuapp.com/api/movies/drama", {
-           headers: {
-             Authorization:
-               "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGFlM2YzMGNlYWY0ODAwMTVjOTE4NjkiLCJpYXQiOjE2MjUwMjYxOTIsImV4cCI6MTYyNjIzNTc5Mn0.PsxaF5WqdL3M99ArdsNnDDq7wUtiPxw_4Nn-WRpsftI",
-           },
-         })
-         const fetchedData=await response.json()
-         displayHorrer(fetchedData)
+         displayMovie(fetchedData,catagory)
          console.log(fetchedData)
    }
 
 window.onload=()=>{
-    let categories=["comedy","drama","horrer"]
-    fetchComedy()
-    fetchDrama()
-    fetchHorrer()
-  
+    fetchMovie("comedy")
+    fetchMovie("drama")
+    fetchMovie("horrer")
+   
 }
